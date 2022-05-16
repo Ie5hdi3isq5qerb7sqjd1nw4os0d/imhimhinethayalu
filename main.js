94b91e3649158b022c344b617974b2949cc8f1a1
SL-Alpha-X <malinduprabanjanaperis@gmail.com>
2022-05-16 21:13:04 +0530

    test

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
 const { default: makeWASocket, useSingleFileAuthState, DisconnectReason, fetchLatestBaileysVersion, generateForwardMessageContent, prepareWAMessageMedia, generateWAMessageFromContent, generateMessageID, downloadContentFromMessage, makeInMemoryStore, jidDecode, proto } = require("@adiwajshing/baileys")
 const { state, saveState } = useSingleFileAuthState(`./auth.json`)
+const events = require("./events")
+const config = require('./config')
+const { Message, Image, Video } = require('./alphaX/')
 const pino = require('pino')
 const { Boom } = require('@hapi/boom')
 const fs = require('fs')
@@ -17,31 +20,8 @@ async function StartAlphaXmd() {
         auth: state
         });
 
-    // anticall auto block
-    hisoka.ws.on('CB:call', async (json) => {
-    const callerId = json.content[0].attrs['call-creator']
-    if (json.content[0].tag == 'offer') {
-    let pa7rick = await hisoka.sendContact(callerId, global.owner)
-    hisoka.sendMessage(callerId, { text: `Sistem otomatis block!\nJangan menelpon bot!\nSilahkan Hubungi Owner Untuk Dibuka !`}, { quoted : pa7rick })
-    await sleep(8000)
-    await hisoka.updateBlockStatus(callerId, "block")
-    }
-    })
-
-    hisoka.ev.on('messages.upsert', async chatUpdate => {
-        console.log(JSON.stringify(chatUpdate, undefined, 2))
-        try {
-        mek = chatUpdate.messages[0]
-        if (!mek.message) return
-        mek.message = (Object.keys(mek.message)[0] === 'ephemeralMessage') ? mek.message.ephemeralMessage.message : mek.message
-        if (mek.key && mek.key.remoteJid === 'status@broadcast') return
-        if (!hisoka.public && !mek.key.fromMe && chatUpdate.type === 'notify') return
-        if (mek.key.id.startsWith('BAE5') && mek.key.id.length === 16) return
-        m = smsg(hisoka, mek, store)
-        require("./hisoka")(hisoka, m, chatUpdate, store)
-        } catch (err) {
-            console.log(err)
-        }
+    AlphaXSock.ev.on('messages.upsert', async msg => {
+        console.log(JSON.stringify(msg, undefined, 2))
     })
 }
 
